6042b2457827ba628f848b6880047a9d98ca7833
Балакина Екатерина Петровна <114657505+BalakinaEP@users.noreply.github.com>
2025-11-06 11:10:25 +0300

    Update задание 1.cpp

--- задание 1.cpp
+++ задание 1.cpp
@@ -2,12 +2,33 @@
 #include <cmath>
 
 using namespace std;
+/**
+* @brief рассчитывает значение функции A по заданной формуле
+* @param x - значение константы x
+* @param y - значение константы y
+* @param z - значение константы z
+* @return - рассчитанное значение
+*/
+float getA(const float x, const float y, const float z);
 
+/**
+* @brief рассчитывает значение функции B по заданной формуле
+* @param x - значение константы x
+* @param y - значение константы y
+* @param z - значение константы z
+* @return - рассчитанное значение
+*/
+float getB(const float x, const float y, const float z);
+
+/**
+* @brief точка входа в программу
+* @return - 0, если программа выполнена корректно
+*/
 int main() {
     //константы
-    float x = 2.2;
-    float y = 9.2;
-    float z = 10.2;
+    const float x = 2.2;
+    const float y = 9.2;
+    const float z = 10.2;
     
     //данных
     cout << "Исходные данные:" << endl;
@@ -15,19 +36,21 @@ int main() {
     cout << "y = " << y << endl;
     cout << "z = " << z << endl;
     cout << endl;
-    
-    //значение a
-    float a = log(z + x*x) + pow(sin(x / y), 2);
-    
-    //значение b
-    float n = exp(-z) * (x + sqrt(x + z));
-    float d = x - sqrt(fabs(x - y));
-    float b = n / d;
-    
+ 
     //результаты
     cout << "Результаты вычислений:" << endl;
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
+    cout << "a = " << getA(x,y,z) << endl;
+    cout << "b = " << getB(x,y,z) << endl;
     
     return 0;
+}
+
+float getA(const float x, const float y, const float z)
+{
+    return log(z + x*x) + pow(sin(x / y), 2);
+}
+
+float getB(const float x, const float y, const float z)
+{
+    return exp(-z) * (x + sqrt(x + z))/(x - sqrt(fabs(x - y)));
 }
