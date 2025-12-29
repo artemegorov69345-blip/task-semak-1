090a234f716e7f83eb3d128b438d5e63d853e50c
Артем Егоров ТКИ-242 <artemegorov69345@gmail.com>
2025-12-29 13:47:14 +0300

    Update Task31.cpp
    исправил

--- Task31.cpp
+++ Task31.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <limits>
 using namespace std;
 
 /**
@@ -8,7 +9,7 @@ using namespace std;
 * @param x - входное значение
 * @return значение функции y в точке x
 */
-double vY(double x);
+double vY(const double x);
 
 /**
 * @brief Проверяет правильность ввода числового значения
@@ -23,7 +24,7 @@ bool checkValue(double &value);
 * @param paramName - название параметра для сообщения об ошибке
 * @return true если значение положительное, false в противном случае
 */
-bool checkPositive(double value, const string &paramName);
+bool checkPositive(const double value, const string &paramName);
 
 /**
 * @brief Проверяет корректность интервала
@@ -31,7 +32,7 @@ bool checkPositive(double value, const string &paramName);
 * @param end - конец интервала
 * @return true если интервал корректен, false в противном случае
 */
-bool checkInterval(double start, double end);
+bool checkInterval(const double start, const double end);
 
 /**
 * @brief Вычисляет значение функции рекуррентным методом
@@ -40,7 +41,15 @@ bool checkInterval(double start, double end);
 * @param n - количество итераций
 * @return значение функции на n-ом шаге
 */
-double getRecurent(double x, double h, int n);
+double getRecurent(const double x, const double h, const int n);
+
+/**
+* @brief Выводит таблицу значений функции на заданном интервале
+* @param start - начало интервала
+* @param end - конец интервала
+* @param step - шаг табулирования
+*/
+void printTable(const double start, const double end, const double step);
 
 /**
 * @brief Основная функция программы
@@ -57,27 +66,30 @@ double getRecurent(double x, double h, int n);
 * @return 0 при успешном выполнении, 1 при ошибке ввода
 */
 int main() {
-    double n, k, h;
+    double n = 0.0, k = 0.0, h = 0.0;
     
     // Ввод параметров табулирования
     cout << "=== Табулирование функции y = 3x - 14 + e^x - e^(-x) ===" << endl;
     
     // Ввод начала интервала
     cout << "Введите начало интервала: ";
-    while (!checkValue(n)) {
-        cout << "Ошибка! Нужно вводить только числа! Попробуйте снова: ";
+    if (!checkValue(n)) {
+        cout << "Ошибка! Нужно вводить только числа!" << endl;
+        return 1;
     }
     
     // Ввод конца интервала
     cout << "Введите конец интервала: ";
-    while (!checkValue(k)) {
-        cout << "Ошибка! Нужно вводить только числа! Попробуйте снова: ";
+    if (!checkValue(k)) {
+        cout << "Ошибка! Нужно вводить только числа!" << endl;
+        return 1;
     }
     
     // Ввод шага табулирования
     cout << "Введите шаг табулирования: ";
-    while (!checkValue(h)) {
-        cout << "Ошибка! Нужно вводить только числа! Попробуйте снова: ";
+    if (!checkValue(h)) {
+        cout << "Ошибка! Нужно вводить только числа!" << endl;
+        return 1;
     }
     
     // Проверка корректности введенных значений
@@ -91,17 +103,18 @@ int main() {
     
     // Выводим заголовок таблицы
     cout << endl << "Таблица значений функции" << endl;
+    
+    // Табулирование функции
+    int t = 0;
+    double x = n;
+    
+    cout << fixed << setprecision(4);
+    
     cout << "+-------------+-------------+" << endl;
     cout << "|     x       |     y       |" << endl;
     cout << "+-------------+-------------+" << endl;
     
-    // Табулирование функции
-    double x = n;
-    int t = 0;
-    
-    cout << fixed << setprecision(4);
-    
-    while (x <= k + h/1000) {
+    while (x <= k) {
         t++;
         double y = vY(x);
         
@@ -127,30 +140,7 @@ int main() {
     // Тестовый пример из задания
     cout << endl << "=== Тестовый пример из задания ===" << endl;
     cout << "Интервал: [1, 3], шаг: 0.2" << endl;
-    
-    double test_n = 1.0;
-    double test_k = 3.0;
-    double test_h = 0.2;
-    
-    cout << "+-------------+-------------+" << endl;
-    cout << "|     x       |     y       |" << endl;
-    cout << "+-------------+-------------+" << endl;
-    
-    x = test_n;
-    while (x <= test_k + test_h/1000) {
-        double y = vY(x);
-        cout << "| " << setw(11) << x << " | ";
-        
-        if (!isfinite(y)) {
-            cout << setw(11) << "Нет решения" << " |" << endl;
-        } else {
-            cout << setw(11) << y << " |" << endl;
-        }
-        
-        x += test_h;
-    }
-    
-    cout << "+-------------+-------------+" << endl;
+    printTable(1.0, 3.0, 0.2);
     
     return 0;
 }
@@ -176,7 +166,7 @@ bool checkValue(double &value) {
 * @param paramName - название параметра для сообщения об ошибке
 * @return true если значение положительное, false в противном случае
 */
-bool checkPositive(double value, const string &paramName) {
+bool checkPositive(const double value, const string &paramName) {
     if (value <= 0) {
         cout << "Ошибка! " << paramName << " должен быть положительным числом!" << endl;
         return false;
@@ -190,7 +180,7 @@ bool checkPositive(double value, const string &paramName) {
 * @param end - конец интервала
 * @return true если интервал корректен, false в противном случае
 */
-bool checkInterval(double start, double end) {
+bool checkInterval(const double start, const double end) {
     if (start > end) {
         cout << "Ошибка! Начало интервала должно быть меньше или равно концу!" << endl;
         return false;
@@ -205,7 +195,7 @@ bool checkInterval(double start, double end) {
 * @param n - количество итераций
 * @return значение функции на n-ом шаге
 */
-double getRecurent(double x, double h, int n) {
+double getRecurent(const double x, const double h, const int n) {
     double currentX = x;
     for (int i = 0; i < n; i++) {
         currentX += h;
@@ -218,6 +208,34 @@ double getRecurent(double x, double h, int n) {
 * @param x - входное значение
 * @return значение функции y в точке x
 */
-double vY(double x) {
+double vY(const double x) {
     return 3 * x - 14 + exp(x) - exp(-x);
+}
+
+/**
+* @brief Выводит таблицу значений функции на заданном интервале
+* @param start - начало интервала
+* @param end - конец интервала
+* @param step - шаг табулирования
+*/
+void printTable(const double start, const double end, const double step) {
+    cout << "+-------------+-------------+" << endl;
+    cout << "|     x       |     y       |" << endl;
+    cout << "+-------------+-------------+" << endl;
+    
+    double x = start;
+    while (x <= end) {
+        double y = vY(x);
+        cout << "| " << setw(11) << x << " | ";
+        
+        if (!isfinite(y)) {
+            cout << setw(11) << "Нет решения" << " |" << endl;
+        } else {
+            cout << setw(11) << y << " |" << endl;
+        }
+        
+        x += step;
+    }
+    
+    cout << "+-------------+-------------+" << endl;
 }
